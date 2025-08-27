756448e5999592b046603e61129aea584db7bb93
Dev Shankar N <30773393+dvshnkr@users.noreply.github.com>
2025-08-28 04:55:36 +0530

    list all tables in db

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,11 +1,18 @@
 import os
+import sqlite3
+import pandas as pd
 from config import get_data_path
 
 # Get the full path to the raw data file
-data_dir = get_data_path()
-print(data_dir)
+data_file = get_data_path("raw", "flexify.sqlite")
 
-# os walk
-for dirpath, dirnames, filenames in os.walk(data_dir):
-    for filename in filenames:
-        print(os.path.join(dirpath, filename))
+# Connect to a database file named 'example.db'
+with sqlite3.connect(data_file) as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    for table in tables:
+        print(f"Printing head of the `{table[0]}` table:")
+        df = pd.read_sql_query(f"SELECT * FROM {table[0]}", conn)
+        print(df.head())
+        print("\n\n")
